2ec9d9b33380622dc8abdb812425ed90669ccd6b
Binyok <164721986+Binyok@users.noreply.github.com>
2024-07-25 21:22:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Tambahkan animasi smooth scroll untuk navigasi
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -6,9 +5,19 @@ document.querySelectorAll('nav a').forEach(anchor => {
         const sectionID = this.getAttribute('href').substring(1);
         const section = document.getElementById(sectionID);
 
+        const headerOffset = document.querySelector('header').offsetHeight;
+        const sectionPosition = section.offsetTop - headerOffset;
+
         window.scrollTo({
             behavior: 'smooth',
-            top: section.offsetTop - 50 // Sesuaikan dengan tinggi header jika ada fixed header
+            top: sectionPosition
         });
     });
+});
+const homeSection = document.getElementById('home');
+const homeBackground = homeSection.style.backgroundImage;
+
+window.addEventListener('scroll', function() {
+    const offset = window.pageYOffset;
+    homeSection.style.backgroundPositionY = offset * 0.5 + 'px'; // Sesuaikan kecepatan efek parallax
 });
